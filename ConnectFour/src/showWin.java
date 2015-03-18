470b194e0f815cd55f3b1560cee7c7d730dbbc9d
mayaramamurthy <https://mayaramamurthy@bitbucket.org/thinkchat/inkchat.git>
2015-03-17 23:43:23 -0400

    jar file

--- ConnectFour/src/showWin.java
+++
@@ -1,8 +0,0 @@
-
-// this class is used by class Check
-// strictly GUI 
-
-public class showWin {
-	// if a game is won, a dot is displayed on the center of each of the four winning discs    
-	
-}
